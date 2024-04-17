108229e933cb15b3b19bf026cf6a0327eb12288c
Dang-1312 <minhdangnc@gmail.com>
2024-04-17 12:19:29 +0700

    Update delete project_app and manage.py in cloud
    Delete project_app and manage.py in cloud (Out of Django project)

--- cloud/Passionfruit_project/project_app/apps.py
+++ cloud/Passionfruit_project/project_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransAwsIotConfig(AppConfig):
+class Project_appConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'project_app'
